e1f15eb3cc6fb0a317cc8432e147908edc21029b
havish10 <netlahavish@gmail.com>
2018-06-21 10:49:06 -0400

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,10 @@
+const Layout = ({ children }) => (
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+    />
+    {children}
+  </div>
+)
+export default Layout
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Jumbotron,
+  ListGroup,
+  ListGroupItem,
+} from 'reactstrap'
+import Layout from '../components/Layout'
+
+const Index = () => (
+  <Layout class="body">
+    <Button outline color="danger">
+      danger
+    </Button>
+  </Layout>
+)
+
+export default Index
